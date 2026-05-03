c05f127b29cdb38ccc358d86e233a41bc3f138a6
ZhuChongLin <8210856@qq.com>
2026-05-03 22:03:36 +0800

    feat(search): 新增内容高亮功能
    - 实现 highlight 函数，用于对指定内容中的查询词进行高亮显示
    - 支持自定义高亮标签，默认使用 <mark> 标签
    - 添加全面的单元测试，覆盖基本功能、边界情况和特殊字符处理
    - 在搜索插件的 API 集中导出 highlight 函数
    - 在主入口文件中引入并导出 highlight 函数，方便外部使用

--- packages/vita-press-plugin-search/src/apis/index.ts
+++ packages/vita-press-plugin-search/src/apis/index.ts
@@ -1,1 +1,2 @@
 export { cancelSearch, search, searchWithIndex } from './search.js'
+export { highlight } from './highlight.js'
--- packages/vita-press-plugin-search/src/index.ts
+++ packages/vita-press-plugin-search/src/index.ts
@@ -1,3 +1,3 @@
 export type * from './shared/types.js'
-export { cancelSearch, search, searchWithIndex } from './apis/index.js'
+export { cancelSearch, search, searchWithIndex, highlight } from './apis/index.js'
 export { searchPlugin, type SearchPluginOptions } from './server/index.js'
