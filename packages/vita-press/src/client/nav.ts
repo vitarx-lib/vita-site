181fe97839e01690a8a9864c0840ad933f197b50
ZhuChongLin <8210856@qq.com>
2026-05-08 01:05:33 +0800

    feat(client): 添加导航树导出和useNavTree函数
    - 在client入口文件中导出nav模块
    - 新增useNavTree函数以返回导航树数据
    - 导入类型NavTree用于函数返回值声明
    - 添加函数注释说明功能和返回值类型

--- packages/vita-press/src/client/nav.ts
+++ packages/vita-press/src/client/nav.ts
@@ -1,4 +1,11 @@
 import navTree from 'virtual:vitapress/runtime/nav'
-export type * from '../types/nav.js'
+import type { NavTree } from '../types/index.js'
 
-export { navTree }
+/**
+ * 获取导航树
+ *
+ * @returns {NavTree} 导航树
+ */
+export function useNavTree(): NavTree {
+  return navTree
+}
